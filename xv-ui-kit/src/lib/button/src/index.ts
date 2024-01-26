625b2d7e8c5308c28de2b5061f5ecbb9418e1711
KSarmitha <ksharmitha95@gmail.com>
2024-01-26 16:26:19 +0530

    loadable component added

--- xv-ui-kit/src/lib/button/src/index.ts
+++ xv-ui-kit/src/lib/button/src/index.ts
@@ -1,18 +1,2 @@
-
-import { NgModule } from '@angular/core';
-import { XvuiButtonDirective } from './lib/xvui-button.directive';
-import { XvuiLoadableButtonComponent } from './lib/xvui-loadable-button.component';
-
 export * from './lib/xvui-button.directive';
 export * from './lib/xvui-loadable-button.component';
-
-export const XvuiButtonImports = [
-	XvuiButtonDirective,
-	XvuiLoadableButtonComponent
-] as const;
-
-@NgModule({
-	imports: [...XvuiButtonImports],
-	exports: [...XvuiButtonImports],
-})
-export class XvuiButtonModule {}
--- xv-ui-kit/src/lib/button/src/lib/xvui-loadable-button.component.ts
+++ xv-ui-kit/src/lib/button/src/lib/xvui-loadable-button.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { XvuiButtonDirective } from './xvui-button.directive';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'xvui-loadable-button',
+  selector: 'button[xvui-loadable-button]',
   standalone: true,
-  imports: [],
-  template: `<p>loadable-button works!</p>`,
-  styles: ``,
+  imports: [CommonModule, XvuiButtonDirective],
+  template: `
+  @if (isLoading) {
+      <div disabled  class="flex mr-4">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Floading&psig=AOvVaw0hdEVpBk7NARv9yoS6Xxq_&ust=1705745082308000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKDgyOGZ6YMDFQAAAAAdAAAAABAE" class="mr-2 animate-spin"/> 
+      Please wait
+    </div>
+    } 
+    @else {
+      <ng-content></ng-content>
+    }
+    `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class XvuiLoadableButtonComponent {}
+export class LoadableButtonComponent {
+  @Input() isLoading = false;
+}
