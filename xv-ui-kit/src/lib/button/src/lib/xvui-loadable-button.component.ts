e25404d480dbd471f243ddbf927315491a099fdd
Sarmitha Krishnagobal <sarmitha.krishnagobal_ext@michelin.com>
2024-01-30 17:15:49 +0530

    fix build issue in root level

--- xv-ui-kit/src/lib/button/src/lib/xvui-loadable-button.component.ts
+++ xv-ui-kit/src/lib/button/src/lib/xvui-loadable-button.component.ts
@@ -4,12 +4,12 @@ import { XvuiButtonDirective } from './xvui-button.directive';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'button[xvui-loadable-button]',
+  selector: 'button[xvui-loadable-button]', //TODO: button[xvui-loadable-button][][xvui-button]
   standalone: true,
   imports: [CommonModule, XvuiButtonDirective],
   template: `
   @if (isLoading) {
-      <div disabled  class="flex mr-4">
+      <div  class="flex mr-4">
       <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Floading&psig=AOvVaw0hdEVpBk7NARv9yoS6Xxq_&ust=1705745082308000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKDgyOGZ6YMDFQAAAAAdAAAAABAE" class="mr-2 animate-spin"/> 
       Please wait
     </div>
@@ -19,7 +19,8 @@ import { XvuiButtonDirective } from './xvui-button.directive';
     }
     `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  styles: []
 })
 export class LoadableButtonComponent {
   @Input() isLoading = false;
---
+++ xv-ui-kit/src/lib/table/README.md
@@ -0,0 +1,3 @@
+# xv-ui-kit/src/lib/table
+
+Secondary entry point of `xv-ui-kit`. It can be used by importing from `xv-ui-kit/src/lib/table`.
