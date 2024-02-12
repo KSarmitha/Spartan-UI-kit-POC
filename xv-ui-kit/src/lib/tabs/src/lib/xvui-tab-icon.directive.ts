3b10b50aa50dc300920add4c81dfc3cd370948a5
gaberathna <147603769+gaberathna@users.noreply.github.com>
2024-02-12 17:20:04 +0530

    tab height was added

--- xv-ui-kit/src/lib/tabs/src/lib/xvui-tab-icon.directive.ts
+++ xv-ui-kit/src/lib/tabs/src/lib/xvui-tab-icon.directive.ts
@@ -1,11 +1,14 @@
-import { computed, Directive, ElementRef, inject, Input, Renderer2 } from '@angular/core';
+import { computed, Directive, ElementRef, inject, Input, Renderer2, signal } from '@angular/core';
 import { BrnTabsDirective } from './brn-tabs.directive';
 import { host } from '@nx/angular/generators';
+import { hlm } from '@spartan-ng/ui-core';
+import { ClassValue } from 'clsx';
 
 @Directive({
   selector: '[xvuiTabIcon]',
   standalone: true,
   host: {
+    '[class]': '_computedClass()',
     '[src]': '_isSelected() ?  inActiveIcon : activeIcon'
   }
 })
@@ -15,6 +18,8 @@ export class XvuiTabIconDirective{
   private readonly _root = inject(BrnTabsDirective);
   private _key: string | undefined;
   protected readonly _isSelected = computed(() => this._root.$value() === this._key);
+  protected _computedClass = computed(() => this._generateClass());
+  private readonly _userCls = signal<ClassValue>('');
   @Input() set appImageSrc(src: string) {
     // console.log(this._isSelected());
     // console.log(this._key);
@@ -34,7 +39,17 @@ export class XvuiTabIconDirective{
     this._key = key;
   }
 
+
+
   constructor(private el: ElementRef, private renderer: Renderer2) {
     // this.renderer.setAttribute(this.el.nativeElement, 'src', this.selected() ? this.activeIcon : this.inActiveIcon);
   }
+
+  private _generateClass() {
+    return hlm(
+      'mr-2',
+      this._userCls(),
+    );
+  }
+
 }
